27815c0712483e1b463262fbaa53de9bbd20f205
ksindorf000 <ksindorf000@gmail.com>
2017-02-08 12:01:27 -0500

    Added Reviews

---
+++ Day3MVCTemplates/Migrations/201702081631296_Initial.cs
@@ -0,0 +1,29 @@
+namespace Day3MVCTemplates.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Reviews",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ReviewerName = c.String(),
+                        Body = c.String(),
+                        Rating = c.Int(nullable: false),
+                        Created = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Reviews");
+        }
+    }
+}
---
+++ Day3MVCTemplates/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel;
+
+namespace Day3MVCTemplates.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        [DisplayName("What is your name?")]
+        public string ReviewerName { get; set; }
+
+        [DisplayName("How was your experience?")]
+        public string Body { get; set; }
+        public int Rating { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public class ReviewContext : DbContext
+    {
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
